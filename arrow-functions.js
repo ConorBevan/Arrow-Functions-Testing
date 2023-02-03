75dbe88854609c33f82238718be5f741a8003216
ConorBevan <conor.bevan@hotmail.com>
2023-02-03 17:26:26 +0000

    test

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,34 @@
  */
 
 // Vanilla JavaScript Function
+/* function addTwoNumbers(a, b) {
+    return a + b; 
+}
+
+let sum = addTwoNumbers(3,5);
+console.log(sum);
+*/
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b; 
+}
+
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid 
+const addTwoNumbers2 = (a, b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message)
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('Hello');
 
 // Returning Multiple Lines
+const returnMultiplelines = () => {
+    
+}
+
